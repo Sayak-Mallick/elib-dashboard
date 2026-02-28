a75913d11e45911f3c5fcfcaead1afaba4ad5e65
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-28 13:11:47 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// ts-ignore
+import { Button } from "./components/ui/button"
+
+function App() {  
+  return (
+    <div>
+      <h1>Welcome to E-LIB DashBoard</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
