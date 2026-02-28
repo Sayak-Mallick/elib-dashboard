704f24b1ff439ce81136f86cab144a9a945a8346
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-28 13:41:55 +0530

    in progress

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />, 
+  },
+]);
