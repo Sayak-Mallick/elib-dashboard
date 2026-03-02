a08a90cf2dcabdd28f4ffb0af59e656a4ea105f4
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-02 05:53:40 +0530

    [TASK] : .env config added

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,10 @@
-// eslint-disable-next-line
-// @ts-ignore
+/* eslint-disable */
+// @ts-nocheck
 import app from "./src/app.ts";
+import { config } from "./src/config/config.ts";
 
 const startServer = async() => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
