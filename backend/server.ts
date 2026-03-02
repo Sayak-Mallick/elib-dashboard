b5787a1b40250f9daf31605be5003f250bac2db3
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-02 05:33:10 +0530

    [TASK] : Express setup completed

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,13 @@
-console.log("Welcome to ebook lirary apis");
+// eslint-disable-next-line
+// @ts-ignore
+import app from "./src/app.ts";
+
+const startServer = async() => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+  });
+};
+
+startServer();
