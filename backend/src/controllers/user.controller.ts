6de537b4d86e52779c17ba92c450550aafbd35f7
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-03 09:53:02 +0530

    [REFACTOR] :  Advanced error handling

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import bcyrpt from "bcrypt";
 import userModel from "../models/user.model";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "../types/user.types";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -17,33 +18,48 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // TODO: in the next version we will implement express-validator for better validation
 
   // Database call to check if user already exists
-  const user = await userModel.findOne({ email: email });
-  if (user) {
-    const error = createHttpError(400, '🔁 User with this email already exists');
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const error = createHttpError(400, '🔁 User with this email already exists');
+      return next(error);
+    }
+  } catch (err) {
+    return next(createHttpError(500, 'Internal server error while checking for existing user'));
   }
+  
 
   // Password hashing
   const hashedPassword = await bcyrpt.hash(password, 10);
 
   // Process the registration logic (e.g., save user to database, hash password, etc.)
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, 'Internal server error while creating user'));
+  }
 
   // JWT token generation for authentication
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, { 
-    expiresIn: '7d',
-    algorithm: 'HS256',
-  });
+  try {
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: '7d',
+      algorithm: 'HS256',
+    });
 
-  // Response with success message or error if registration fails
-  res.json({
-    accessToken: token,
-    message: '✅ User registered successfully',
-  });
+    // Response with success message or error if registration fails
+    res.json({
+      accessToken: token,
+      message: '✅ User registered successfully',
+    });
+  } catch (error) {
+    return next(createHttpError(500, 'Internal server error while generating token'));
+  }
+  
 }
 
 export { createUser };
