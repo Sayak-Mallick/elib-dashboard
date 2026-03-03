01868407d399085997bef90f593af625e8f4c5c4
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-03 07:11:44 +0530

    [TASK] : Added user model

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,17 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "../models/user.model";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
 
   // validation
   if (!name || !email || !password) {
     const error = createHttpError(400, 'All fields are required');
     return next(error);
   }  // we can use express-validator for better validation
-   
+
   // TODO: in the next version we will implement express-validator for better validation
 
+  // Database call to check if user already exists
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, '🔁 User with this email already exists');
+    return next(error);
+  }
+  // Process the registration logic (e.g., save user to database, hash password, etc.)
+  // Response with success message or error if registration fails
   return res.json({
     message: '✅ User registered successfully',
   });
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { User } from "../types/user.types";
+
+const userSchema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+
+// users
+export default mongoose.model<User>('User', userSchema);
